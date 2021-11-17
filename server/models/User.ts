4b15b735923fe073c1ae186c4a2068433f549124
diegomezcota <gomezcotadiego@gmail.com>
2021-11-17 16:36:24 -0600

    Change User class to mongo schema

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,40 +1,19 @@
 import { uuid } from "./types";
+const mongoose = require('mongoose')
 
-class User {
-  private user_uuid: uuid;
-  public username: string;
-  private password: string;
-  private email: string;
-  private gender: string;
-  private points_of_contact: string;
-  private trip_wishlist: uuid[];
+const Schema = mongoose.Schema 
 
-  constructor(user_uuidC: uuid, usernameC: string, passwordC: string, emailC: string, genderC: string, points_of_contactC: string, trip_wishlistC: uuid[]) {
-    this.user_uuid = user_uuidC;
-    this.username = usernameC;
-    this.password = passwordC;
-    this.email = emailC;
-    this.gender = genderC;
-    this.points_of_contact = points_of_contactC;
-    this.trip_wishlist = trip_wishlistC;
-  }
+const UserSchema = new Schema(
+  {
+    user_uuid: String,
+    username: String,
+    password: String,
+    email: String,
+    gender: String,
+    points_of_contact: String,
+    trip_wishlist: [String],
+  },
+  { timestamps: true }
+);
 
-  get_trip_wishlist(): uuid[] {
-    return this.trip_wishlist;
-  }
-
-  get_email(): string {
-    return this.email;
-  }
-
-  get_gender(): string {
-    return this.gender;
-  }
-
-  get_points_of_contact(): string {
-    return this.points_of_contact;
-  }
-
-}
-
-export default User;
+module.exports = mongoose.model('User', UserSchema);
