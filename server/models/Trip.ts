9b497f068a8acfd8a20c2b739b9442f50d0aa6d2
diegomezcota <gomezcotadiego@gmail.com>
2021-11-17 17:34:39 -0600

    Change trip to follow mongo schema

--- server/models/Trip.ts
+++ server/models/Trip.ts
@@ -1,43 +1,67 @@
+import { stringify } from "node:querystring";
 import { uuid } from "./types";
+const mongoose = require('mongoose')
 
-class Trip {
+const Schema = mongoose.Schema 
 
-    private trip_uuid: uuid;
-    private user_uuid: uuid;
-    public destination: string;
-    private activities_preference: Set<string>;
-    private gender_preference: Set<string>;
-    private date_range: Set<Date>;
+const TripSchema = new Schema(
+    {
+        trip_uuid: String,
+        user_uuid: String,
+        destination: String,
+        activities_preference: { 
+            type: Map, 
+            of: String
+        },
+        gender_preference: { 
+            type: Map, 
+            of: String
+        },
+        date_range: [String] // could be changed to array or map of dates
+    },
+    { timestamps: true }
+);
 
-    constructor(trip_uuidC: uuid, user_uuidC: uuid, 
-        destinationC: string, activities_preferenceC: Set<string>,
-        gender_preferenceC: Set<string>, date_rangeC: Set<Date>){
+module.exports = mongoose.model('Trip', TripSchema);
+
+// class Trip {
+
+//     private trip_uuid: uuid;
+//     private user_uuid: uuid;
+//     public destination: string;
+//     private activities_preference: Set<string>;
+//     private gender_preference: Set<string>;
+//     private date_range: Set<Date>;
+
+//     constructor(trip_uuidC: uuid, user_uuidC: uuid, 
+//         destinationC: string, activities_preferenceC: Set<string>,
+//         gender_preferenceC: Set<string>, date_rangeC: Set<Date>){
         
-            this.trip_uuid = trip_uuidC;
-            this.user_uuid = user_uuidC;
-            this.destination = destinationC;
-            this.activities_preference = activities_preferenceC;
-            this.gender_preference = gender_preferenceC;
-            this.date_range = date_rangeC;
+//             this.trip_uuid = trip_uuidC;
+//             this.user_uuid = user_uuidC;
+//             this.destination = destinationC;
+//             this.activities_preference = activities_preferenceC;
+//             this.gender_preference = gender_preferenceC;
+//             this.date_range = date_rangeC;
 
-    }
+//     }
 
-    request_matches(): string {
-        //aver aquí va algo con match
-        return "matches";
-    }
+//     request_matches(): string {
+//         //aver aquí va algo con match
+//         return "matches";
+//     }
 
-    get_activities_preference(): Set<string> {
-        return this.activities_preference;
-    }
+//     get_activities_preference(): Set<string> {
+//         return this.activities_preference;
+//     }
 
-    get_gender_preference(): Set<string> {
-        return this.gender_preference;
-    }
+//     get_gender_preference(): Set<string> {
+//         return this.gender_preference;
+//     }
 
-    get_date_range(): Set<Date> {
-        return this.date_range;
-    }
-}
+//     get_date_range(): Set<Date> {
+//         return this.date_range;
+//     }
+// }
 
-export default Trip;
+// export default Trip;
