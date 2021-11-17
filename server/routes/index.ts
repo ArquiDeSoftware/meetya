640160c222c2f88b94a129533e35900fa6e98ee5
diegomezcota <gomezcotadiego@gmail.com>
2021-11-17 17:35:01 -0600

    Add addTrip route for post

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { addUser } from '../controllers/userController'
+import { addTrip } from '../controllers/tripController'
 
 const api = Router();
 
 // access this by localhost:9000/api
 
 api.post('/users', addUser)
+api.post('/trips', addTrip)
 
 api.get('/', (req, res) => {
     res.send('Welcome to APIs!');
