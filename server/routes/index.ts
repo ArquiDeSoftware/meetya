5f2245ba8d700585d0017c6ada0e7a7b93884628
diegomezcota <gomezcotadiego@gmail.com>
2021-11-17 16:36:47 -0600

    Add api route that adds user

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,6 +1,11 @@
 import { Router } from 'express';
+import { addUser } from '../controllers/userController'
 
 const api = Router();
+
+// access this by localhost:9000/api
+
+api.post('/users', addUser)
 
 api.get('/', (req, res) => {
     res.send('Welcome to APIs!');
