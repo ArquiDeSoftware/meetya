4f8041549184645a1b943c34489012e9e0cbd496
JorgeGiovannetti <giovannetti2000@gmail.com>
2021-11-18 17:57:55 -0600

    Create trip form submit

--- src/pages/components/createTrip/create_trip.jsx
+++ src/pages/components/createTrip/create_trip.jsx
@@ -1,130 +1,194 @@
-import React from "react";
+import React, { useState } from "react";
 import "./create_trip.css";
+import { useForm } from "react-hook-form";
+// import DatePicker from "react-datepicker";
+// import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+
 export default function CreateTrip() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    const formattedData = {
+      username: "diegomez",
+      destination: data.location,
+      activities_preference: {
+        landmarks: data.aLandmarks,
+        tours: data.aTours,
+        cultural: data.cultural,
+        party: data.party,
+        localCuisine: data.aLocalCuisine,
+        outdoors: data.aOutdoors,
+      },
+      gender_preference: {
+        men: data.gMen,
+        women: data.women,
+        nonBinary: data.nonBinary,
+      },
+      start_date: startDate,
+      end_date: endDate,
+    };
+
+    console.log(formattedData);
+  };
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
   return (
     <>
       <link rel="stylesheet" href="create_trip.css"></link>
-      <div class='container'>
-        <div class='text-center card'>
-          <div class='container form-signin bg-light'>
-            <form>
-              <h1 class='h3 mb-3 fw-normal'>Create your next trip! </h1>
+      <div className="container">
+        <div className="text-center card">
+          <div className="container form-signin bg-light">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <h1 className="h3 mb-3 fw-normal">Create your next trip! </h1>
 
-              <div class='form-floating'>
-                <label class='text-secondary' for='floatingLocation'>
+              <div className="form-floating">
+                <label className="text-secondary" htmlFor="floatingLocation">
                   Location
                 </label>
                 <br></br>
                 <input
-                  type='location'
-                  class='form-control'
-                  id='floatingLocation'
-                  placeholder='Dream location'
+                  type="location"
+                  className="form-control"
+                  id="floatingLocation"
+                  placeholder="Dream location"
+                  {...register("location", { required: true })}
                 />
+                {errors.location && <p style={{color: "red"}}>Location is required</p>}
               </div>
               <br></br>
 
-
-              <div class='form-floating'>
-                <label class='text-secondary' for='floatingDateAvailable'>
+              <div className="form-floating">
+                <label className="text-secondary" htmlFor="floatingDateAvailable">
                   Dates available
                 </label>
                 <br></br>
-                <p> First date available: </p>
-                <input
-                  type='text'
-                  class='form-control'
-                  id='floatingDateAvailable'
-                  placeholder='Day'
-                />
-                <input
-                  type='text'
-                  class='form-control'
-                  id='floatingDateAvailable'
-                  placeholder='Month'
-                />
-                <input
-                  type='text'
-                  class='form-control'
-                  id='floatingDateAvailable'
-                  placeholder='Year'
-                />
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                  <KeyboardDatePicker
+                    label="First date available"
+                    value={startDate}
+                    onChange={setStartDate}
+                  />
+                </MuiPickersUtilsProvider>
+
                 <br></br>
-                <p>Last Date Available: </p>
-                <input
-                  type='text'
-                  class='form-control'
-                  id='floatingDateAvailable'
-                  placeholder='Day'
-                />
-                <input
-                  type='text'
-                  class='form-control'
-                  id='floatingDateAvailable'
-                  placeholder='Month'
-                />
-                <input
-                  type='text'
-                  class='form-control'
-                  id='floatingDateAvailable'
-                  placeholder='Year'
-                />
+                <br></br>
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                  <KeyboardDatePicker
+                    label="Last date available"
+                    value={endDate}
+                    onChange={setEndDate}
+                  />
+                </MuiPickersUtilsProvider>
               </div>
               <br></br>
 
+              <label className="text-secondary" htmlFor="floatingPGenero">
+                Gender Preference
+              </label>
+              <br></br>
+              <label>
+                <input type="checkbox" name="gMen" {...register("gMen")} /> Men
+              </label>
+              <br></br>
+              <label>
+                <input type="checkbox" name="gWomen" {...register("gWomen")} />{" "}
+                Women
+              </label>
+              <br></br>
+              <label>
+                <input
+                  type="checkbox"
+                  name="gNonBinary"
+                  {...register("gNonBinary")}
+                />{" "}
+                Non Binary
+              </label>
+              <br></br>
 
-              <div class='form-floating'>
-                <label class='text-secondary' for='floatingPGenero'>
-                  Gender Preference
+              <div className=" mt-4">
+                <label
+                  className="mx-2 text-secondary"
+                  htmlFor="floatingPActivities"
+                >
+                  Activities Preference
                 </label>
                 <br></br>
-                <label><input type="checkbox" name="gMen"/> Men</label><br></br>
-                <label><input type="checkbox" name="gWomen"/> Women</label><br></br>
-                <label><input type="checkbox" name="gNonBinary"/> Non Binary</label><br></br>
-              </div>
-              <br></br>
-
-
-              <div class='form-floating mt-3'>
-                <label class='text-secondary' for='floatingPAge'>
-                  Age Preference
+                <label>
+                  <input
+                    type="checkbox"
+                    name="aLandmarks"
+                    {...register("aLandmarks")}
+                  />{" "}
+                  Landmarks
                 </label>
                 <br></br>
-                <input
-                  type='number'
-                  class='form-control'
-                  id='eMin'
-                  placeholder='Min'
-                />
-
-                <input
-                  type='number'
-                  class='form-control'
-                  id='eMax'
-                  placeholder='Max'
-                />
-              </div>
-              <br></br>
-
-              <div class=' mt-4'>
-                <label class='mx-2 text-secondary' for='floatingPActivities'>
-                    Activities Preference
+                <label>
+                  <input
+                    type="checkbox"
+                    name="aTours"
+                    {...register("aTours")}
+                  />{" "}
+                  Tours
                 </label>
                 <br></br>
-                <label><input type="checkbox" name="aLandmarks"/> Landmarks</label><br></br>
-                <label><input type="checkbox" name="aTours"/> Tours</label><br></br>
-                <label><input type="checkbox" name="aCultural"/> Cultural</label><br></br>
-                <label><input type="checkbox" name="aParty"/> Party</label><br></br>
-                <label><input type="checkbox" name="aLocalCuisine"/> Local Cuisine</label><br></br>
-                <label><input type="checkbox" name="aOutdoors"/> Outdoors</label><br></br>
+                <label>
+                  <input
+                    type="checkbox"
+                    name="aCultural"
+                    {...register("aCultural")}
+                  />{" "}
+                  Cultural
+                </label>
+                <br></br>
+                <label>
+                  <input
+                    type="checkbox"
+                    name="aParty"
+                    {...register("aParty")}
+                  />{" "}
+                  Party
+                </label>
+                <br></br>
+                <label>
+                  <input
+                    type="checkbox"
+                    name="aLocalCuisine"
+                    {...register("aLocalCuisine")}
+                  />{" "}
+                  Local Cuisine
+                </label>
+                <br></br>
+                <label>
+                  <input
+                    type="checkbox"
+                    name="aOutdoors"
+                    {...register("aOutdoors")}
+                  />{" "}
+                  Outdoors
+                </label>
+                <br></br>
               </div>
               <br></br>
 
-              <button class='w-100 mt-4 btn btn-lg btn-primary' type='submit'>
+              <button
+                className="w-100 mt-4 btn btn-lg btn-primary"
+                type="submit"
+              >
                 Create trip
               </button>
             </form>
-
           </div>
         </div>
       </div>
