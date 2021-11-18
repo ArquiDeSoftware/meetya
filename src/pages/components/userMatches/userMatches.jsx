962c6bf3dd31a5c08cb6853dcc349f59e82247ea
Andrea Ramirez <andrearamirezfernandez@gmail.com>
2021-11-18 16:55:21 -0600

    display matches

--- src/pages/components/userMatches/userMatches.jsx
+++ src/pages/components/userMatches/userMatches.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Topbar from "../../../components/topbar/Topbar";
 import "./userMatches.css";
 import React, { useState, useEffect } from 'react';
 import { getMatches } from '../../services/matches';
@@ -22,11 +23,18 @@ export default function UserMatches() {
   
     console.log(matches)
 
+    var html = '<ul>';
+    for (var i = 0; i <= matches.length; i++) {
+        html += '<li>' + matches[i] + '</li>';
+    }
+    html += '</ul>';
+    document.getElementById('listMatches').innerHTML= html;
+
     return (
       <>
         <div class='container'>
             <h3> User Matches</h3>
-
+            <div id='listMatches'></div>
         </div>
       </>
     );
